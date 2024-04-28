0289db91f6cb0c205932ec8ad6274aae072381c0
qinsx <qsx@t-chip.com.cn>
2024-04-28 15:34:56 +0800

    修复: 当.aie-container 里限制max-height时全屏模式受影响问题

--- src/components/menus/Fullscreen.ts
+++ src/components/menus/Fullscreen.ts
@@ -28,6 +28,7 @@ export class Fullscreen extends AbstractMenuButton {
       container.style.top = "0";
       container.style.left = "0";
       container.style.zIndex = "9999";
+      container.style.maxHeight = "100%";
     } else {
       container.style.height = "100%";
       container.style.width = "";
@@ -36,6 +37,7 @@ export class Fullscreen extends AbstractMenuButton {
       container.style.top = "";
       container.style.left = "";
       container.style.zIndex = "";
+      container.style.maxHeight = "";
     }
     this.isFullscreen = !this.isFullscreen;
     this.querySelector("div")!.innerHTML = this.isFullscreen
